c67f16ce005f98268cce828d1c6aa03e479144e8
Malik-Umair-khalid <81845666+Malik-Umair-khalid@users.noreply.github.com>
2021-04-11 23:00:05 -0700

    task 5 completed

--- app.js
+++ app.js
@@ -22,6 +22,46 @@
 //  var result = firstno / secondno
   
 // document.write(result)
+// ********************************************************************************************************************************
+
+// var number1 = 5
+// document.write(number1)
+
+// var number1 = 5
+// document.write(++number1)
+
+// var number1 = 5 + 7
+// document.write(++number1)
+
+// document.write(--number1)
+
+// document.write(12/3)
+// ********************************************************************************************************************
+
+// var ticketprize = 600
+// var buys = 5
+
+// document.write ( ticketprize * buys)
+
+// ******************************************************************************************************************
+
+// var number4 = 4
+// document.write(4*1)
+// document.write(4*2) 
+// document.write(4*3)
+// document.write(4*4)
+// document.write(4*5)
+// document.write(4*6)
+// document.write(4*7)
+// document.write(4*8)
+// document.write(4*9)
+// document.write(4*10)
+
+// *****************************************************************************************************************
+
+
+
+
 
 
 
