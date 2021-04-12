2ab329aec713c3e2db7ad8b84ba20f5b9299b116
Malik-Umair-khalid <81845666+Malik-Umair-khalid@users.noreply.github.com>
2021-04-11 22:27:15 -0700

    Task 2 Completed

--- app.js
+++ app.js
@@ -5,3 +5,23 @@
   
 // document.write(result)
 
+//  var firstno = 3
+//  var secondno = 5
+//  var result = firstno * secondno
+  
+// document.write(result)
+
+//  var firstno = 3
+//  var secondno = 5
+//  var result = firstno - secondno
+  
+// document.write(result)
+
+//  var firstno = 3
+//  var secondno = 5
+//  var result = firstno / secondno
+  
+// document.write(result)
+
+
+
